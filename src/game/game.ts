e3b521275fbf36f69ed680c32f6cae1f9cf3ebf6
matteomiceli <omatteomiceli@gmail.com>
2022-01-02 20:31:47 -0800

    game rules

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,15 @@
-export default class fart {
-  test: string;
+// game 1 prize draw 
+// 0-20 input ==> number of tickets entered into draw
+// winner gets prize of $100
+// prize is divided by the total number of tickets entered by players at the table 
+// 3 rounds, prize increases each time 100, 200, 300
 
-  constructor(test: string) {
-    this.test = test;
-  }
-}
+
+// game 2 lowest bid
+// each player gets 5 paddles 1-5
+// winner is the person holding the lowest number not chosen by any other player
+// prize $20 * winning number
+// 3 rounds prize $20, $50, $100
+
+
+// game 3 
